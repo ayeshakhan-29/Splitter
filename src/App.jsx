e7a00e8905a53d79e28de99080c1e95d063e4c3f
ayeshakhan-29 <aarkk.29@gmail.com>
2024-04-02 16:31:39 +0500

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ export default function App() {
     const finalTipPercentage = customTipPercentage || tipPercentage;
     const bill = parseFloat(billAmount);
     const tip = bill * (finalTipPercentage / 100);
-    const tipPerPerson = tip / parseFloat(numberOfPeople);
+    const tipPerPerson = numberOfPeople === '' || numberOfPeople === '0' ? 0 : tip / parseFloat(numberOfPeople);
     const totalBillWithTip = bill + tip;
-    const billPerPerson = totalBillWithTip / parseFloat(numberOfPeople);
+    const billPerPerson = numberOfPeople === '' || numberOfPeople === '0' ? 0 : totalBillWithTip / parseFloat(numberOfPeople);
     console.log("Bill Amount:", bill);
     console.log("Final Tip Percentage:", finalTipPercentage);
     console.log("Number of People:", numberOfPeople);
@@ -36,9 +36,10 @@ export default function App() {
     };
   };
 
+
   return (
     <div className="h-full w-screen md:w-full md:h-full md:flex md:flex-wrap md:flex-col md:items-center md:justify-around">
-      <div className="text-center pt-4">
+      <div className="text-center md:py-8 py-6">
         <span className="uppercase font-mono text-3xl text-[#114A4E] tracking-widest">spli</span><br />
         <span className="uppercase font-mono text-3xl mt-2 text-[#114A4E] tracking-widest">tter</span>
       </div>
@@ -56,7 +57,7 @@ export default function App() {
               id="bill"
               value={billAmount}
               onChange={(e) => setBillAmount(e.target.value)}
-              className="peer w-full h-full  bg-transparent pl-10 pr-3 sm:text-2xl rounded-md text-right bg-zinc-100 text-[#114A4E] font-sans font-bold outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-lg px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
+              className="peer w-full h-full  bg-transparent pl-10 pr-3 sm:text-2xl rounded-md text-right bg-zinc-100 text-[#114A4E]  font-bold outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-lg px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
               placeholder="0"
             />
           </div>
@@ -78,20 +79,25 @@ export default function App() {
             <button className={`mr-4 px-4 py-2 ${selectedButton === 50 ? 'bg-[#26C2AD] text-[#114A4E]' : 'bg-[#114A4E] text-white'}  text-lg font-bold rounded-md w-[5.5rem] md:w-[6.5rem]`} onClick={() => { setTipPercentage(50); setSelectedButton(50); }}>50%</button>
             <input
               type="text"
-              className="bg-gray-100 border md:text-center text-center text-sm md:text-lg font-semibold  text-[#114A4E] rounded-md px-3 py-2 w-[5.5rem] md:w-[6.5rem] outline outline-0 focus:outline-0 border focus:border-2 focus:border-blue-500' "
+              className="bg-gray-100 border md:text-center text-center text-sm md:text-lg font-semibold text-[#114A4E] rounded-md px-3 py-2 w-[5.5rem] md:w-[6.5rem] outline outline-0 focus:outline-0 border focus:border-2 focus:border-blue-500"
               placeholder="Custom"
               value={customTipPercentage}
               onChange={(e) => {
                 const value = e.target.value;
-                if (/^\d*$/.test(value)) {
+                if (value === '' || (/^\d+$/.test(value) && value >= 1 && value <= 100)) {
                   setCustomTipPercentage(value);
                   setTipPercentage(value);
                 }
               }}
             />
-            {customTipPercentage && parseInt(customTipPercentage) === 0 && (
-              <span className="text-red-500 text-sm font-medium ml-2">can't be zero</span>
+            {customTipPercentage && (parseInt(customTipPercentage) === 0 || parseInt(customTipPercentage) > 100) && (
+              <span className="text-red-500 text-sm font-medium ml-2">Enter a number between 1 and 100</span>
             )}
+
+            {customTipPercentage && (parseInt(customTipPercentage) === 0 || parseInt(customTipPercentage) > 100) && (
+              <span className="text-red-500 text-sm font-medium ml-2">Enter a number between 1 and 100</span>
+            )}
+
           </div>
           {/* Number of People section */}
           <div className="relative mt-1 rounded-md shadow-sm">
@@ -103,7 +109,7 @@ export default function App() {
                 Number of People
               </label>
               {numberOfPeople && parseInt(numberOfPeople) === 0 && (
-                <span className="text-red-500 text-sm font-medium ml-20 md:ml-32">can't be zero</span>
+                <span className="text-red-500 text-sm font-medium ml-24 md:ml-36 md:py-3 py-3">can't be zero</span>
               )}
             </div>
             <input
@@ -117,7 +123,7 @@ export default function App() {
                   setNumberOfPeople(value);
                 }
               }}
-              className={`peer w-full h-full bg-transparent pl-10 pr-3 sm:text-2xl rounded-md text-right bg-zinc-100 text-[#114A4E] font-sans font-bold outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-lg px-3 py-2.5 rounded-[7px] ${numberOfPeople && parseInt(numberOfPeople) === 0 ? 'border-red-500' : 'border-blue-gray-200 focus:border-blue-500'}`}
+              className={`peer w-full h-full bg-transparent pl-10 pr-3 sm:text-2xl rounded-md text-right bg-zinc-100 text-[#114A4E] font-bold outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-lg px-3 py-2.5 rounded-[7px] ${numberOfPeople && parseInt(numberOfPeople) === 0 ? 'border-red-500' : 'border-blue-gray-200 focus:border-blue-500'}`}
               placeholder="0"
             />
           </div>
