0bd710fb843690a3c6ba6cd49fe47d12aab3eacc
ayeshakhan-29 <aarkk.29@gmail.com>
2024-04-02 12:30:14 +0500

    making it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
-import user from "./assets/user.png"
+import user from "./assets/user.png";
+import Hero from "./Components/Sections/Hero";
 
-export default function Card() {
+export default function App() {
+  const defaultTipPerPerson = 0;
+  const defaultBillPerPerson = 0;
+
   const [billAmount, setBillAmount] = useState("");
   const [tipPercentage, setTipPercentage] = useState(0);
   const [numberOfPeople, setNumberOfPeople] = useState("");
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const resetValues = () => {
+    setBillAmount("");
+    setTipPercentage(0);
+    setNumberOfPeople("");
+  };
 
   const calculateTipPerPerson = () => {
     const bill = parseFloat(billAmount);
@@ -24,14 +35,18 @@ export default function Card() {
   };
 
   return (
-    <div className="h-screen">
-      <p>hiiii</p>
-      <div className="flex justify-center bg-white mt-20 mx-[20rem] rounded-lg">
-        <div className="h-full w-full justify-center max-w-sm m-4 p-4">
-          <label htmlFor="bill" className="block text-sm font-medium text-gray-700">Bill</label>
+    <div className="h-screen w-screen">
+      <div className="text-center pt-4">
+        <span className="uppercase font-mono text-3xl text-[#114A4E] tracking-widest">spli</span><br />
+        <span className="uppercase font-mono text-3xl mt-2 text-[#114A4E] tracking-widest">tter</span>
+      </div>
+
+      <div className="h-full grid md:grid-cols-2 bg-white rounded-lg m-3 p-2 ">
+        <div className="h-full w-full justify-center md:max-w-sm p-4">
+          <label htmlFor="bill" className="block text-sm font-medium text-gray-500 ">Bill</label>
           <div className="relative mt-1 rounded-md shadow-sm">
             <div className="absolute inset-y-0 left-0 pl-3 flex pointer-events-none">
-              <span className="text-gray-500 sm:text-sm">$</span>
+              <span className="text-gray-400 mt-2 sm:text-sm">$</span>
             </div>
             <input
               type="text"
@@ -39,68 +54,71 @@ export default function Card() {
               id="bill"
               value={billAmount}
               onChange={(e) => setBillAmount(e.target.value)}
-              className="block h-[2rem] w-full border-transparent focus:border-blue-500 pl-10 pr-3 sm:text-sm rounded-md text-right bg-gray-100 text-custom-color"
+              className="peer w-full h-full bg-transparent pl-10 pr-3 sm:text-2xl rounded-md text-right bg-zinc-100 text-[#114A4E] font-sans font-bold outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-lg px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
               placeholder="0"
             />
           </div>
 
-
           {/* Select Tip section */}
-          <div className="p-8 mt-[-20px] w-[50rem]">
-            <label htmlFor="selectTip" className="block text-sm font-medium text-gray-700 mb-2">Select Tip</label>
-            <div className="flex mb-4">
-              <button className="mr-4 px-4 py-2 bg-[#114A4E] text-white rounded-md w-[5rem]" onClick={() => setTipPercentage(5)}>5%</button>
-              <button className="mr-4 px-4 py-2 bg-[#114A4E] text-white rounded-md w-[5rem]" onClick={() => setTipPercentage(10)}>10%</button>
-              <button className="px-4 py-2 bg-[#114A4E] text-white rounded-md w-[5rem]" onClick={() => setTipPercentage(15)}>15%</button>
-            </div>
-            <div className="flex mb-4">
-              <button className="mr-4 px-4 py-2 bg-[#114A4E] text-white rounded-md w-[5rem]" onClick={() => setTipPercentage(25)}>25%</button>
-              <button className="mr-4 px-4 py-2 bg-[#114A4E] text-white rounded-md w-[5rem]" onClick={() => setTipPercentage(50)}>50%</button>
-              <input type="text" className="bg-gray-100 border border-gray-300 text-[#114A4E] rounded-md px-3 py-2 w-[5rem]" placeholder="Custom" />
-            </div>
+          <label htmlFor="selectTip" className="block text-sm font-medium text-gray-500 my-2">Select Tip</label>
+          <div className="flex mb-4">
+            <button className={`mr-4 px-4 py-2 ${selectedButton === 5 ? 'bg-[#26C2AD]' : 'bg-[#114A4E]'} text-white rounded-md w-[5.5rem]`} onClick={() => { setTipPercentage(5); setSelectedButton(5); }}>5%</button>
+            <button className={`mr-4 px-4 py-2 ${selectedButton === 10 ? 'bg-[#26C2AD]' : 'bg-[#114A4E]'} text-white rounded-md w-[5.5rem]`} onClick={() => { setTipPercentage(10); setSelectedButton(10); }}>10%</button>
+            <button className={`px-4 py-2 ${selectedButton === 15 ? 'bg-[#26C2AD]' : 'bg-[#114A4E]'} text-white rounded-md w-[5.5rem]`} onClick={() => { setTipPercentage(15); setSelectedButton(15); }}>15%</button>
           </div>
-
+          <div className="flex mb-4">
+            <button className={`mr-4 px-4 py-2 ${selectedButton === 25 ? 'bg-[#26C2AD]' : 'bg-[#114A4E]'} text-white rounded-md w-[5.5rem]`} onClick={() => { setTipPercentage(25); setSelectedButton(25); }}>25%</button>
+            <button className={`mr-4 px-4 py-2 ${selectedButton === 50 ? 'bg-[#26C2AD]' : 'bg-[#114A4E]'} text-white rounded-md w-[5.5rem]`} onClick={() => { setTipPercentage(50); setSelectedButton(50); }}>50%</button>
+            <input type="text" className="bg-gray-100 border text-sm border-gray-300 text-[#114A4E] rounded-md px-3 py-2 w-[5.5rem] focus:outline-0" placeholder="Custom" />
+          </div>
           {/* Number of People section */}
-          <div className="p-8 mt-[-3rem]">
-            <label className="block text-sm font-medium text-gray-500">Number of People</label>
-            <div className="relative mt-1 rounded-md shadow-sm">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <img src={user} className="h-[1rem] py-[2px]" alt="user" />
+          <label className="block text-sm font-medium text-gray-500">Number of People</label>
+          <div className="relative mt-1 rounded-md shadow-sm">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <img src={user} className="h-[1rem] py-[2px]" alt="user" />
+            </div>
+            <input
+              type="text"
+              name="numberOfPeople"
+              id="numberOfPeople"
+              value={numberOfPeople}
+              onChange={(e) => setNumberOfPeople(e.target.value)}
+              className="peer w-full h-full bg-transparent pl-10 pr-3 sm:text-2xl rounded-md text-right bg-zinc-100 text-[#114A4E] font-sans font-bold outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-lg px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
+              placeholder="0"
+            />
+          </div>
+        </div>
+
+        <div className="h-full w-full justify-center md:max-w-sm p-4 mt-[-3rem]">
+          {/* Tip Per Person */}
+          <div className=" bg-[#114A4E] rounded-lg">
+            <div className="p-8 flex justify-between gap-4 items-center  ">
+              <div>
+                <label className="block text-sm font-medium text-white">Tip Amount</label>
+                <p className="font-normal text-xs text-gray-100">/ person</p>
               </div>
-              <input
-                type="text"
-                name="numberOfPeople"
-                id="numberOfPeople"
-                value={numberOfPeople}
-                onChange={(e) => setNumberOfPeople(e.target.value)}
-                className="block w-full border-transparent focus:border-blue-500 pl-10 pr-3 sm:text-sm rounded-md text-right bg-gray-100 text-black"
-                placeholder="0"
-              />
+
+              <p className="text-4xl font-semibold text-[#26C2AD]">${calculateTipPerPerson().tipPerPerson}</p>
             </div>
+
+            {/* Bill Per Person */}
+            <div className="px-8 flex justify-between gap-4 items-center">
+              <div>
+                <label className="block text-sm font-medium text-white">Total</label>
+                <p className="font-normal text-xs text-gray-100">/ person</p>
+              </div>
+              <p className="text-4xl font-semibold text-[#26C2AD]">${calculateTipPerPerson().billPerPerson}</p>
+            </div>
+            {/* Reset Button */}
+            <div className="flex justify-center items-center">
+              <button className="m-8 h-[2.5rem] w-[15rem] rounded-lg bg-[#26C2AD]" onClick={resetValues}>RESET</button>
+            </div>
+
           </div>
+
         </div>
 
-        {/* Tip Per Person */}
-        <div className="flex flex-col justify-center w-full bg-[#114A4E] max-w-sm rounded-lg my-4 mx-2">
-          <div class="p-8 flex justify-between gap-4 items-center">
-            <div>
-              <label class="block text-sm font-medium text-white">Tip Amount</label>
-              <p className="font-normal text-xs text-gray-100">/ person</p>
-            </div>
 
-            <p class="text-4xl font-semibold text-[#26C2AD]">${calculateTipPerPerson().tipPerPerson}</p>
-          </div>
-
-          {/* Bill Per Person */}
-          <div className="p-8 flex justify-between gap-4 items-center">
-            <div>
-              <label class="block text-sm font-medium text-white">Total</label>
-              <p className="font-normal text-xs text-gray-100">/ person</p>
-            </div>
-            <p className="text-4xl font-semibold text-[#26C2AD]">${calculateTipPerPerson().billPerPerson}</p>
-          </div>
-          <button className="m-8 bg-[#26C2AD]">RESET</button>
-        </div>
       </div>
     </div>
   );
--- src/Components/Sections/Hero.jsx
+++ src/Components/Sections/Hero.jsx
@@ -1,37 +1,18 @@
 import React from "react";
 
-export default function Card() {
+export default function Hero() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-white border border-black">
-      <div className="p-8 rounded-lg shadow-md">
-        <label htmlFor="bill" className="block text-sm font-medium text-gray-700">Bill</label>
-        <div className="relative mt-1 rounded-md shadow-sm">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <span className="text-gray-500 sm:text-sm">$</span>
-          </div>
-          <input
-            type="text"
-            name="bill"
-            id="bill"
-            className="block w-full border-transparent focus:border-blue-500 pl-10 pr-3 sm:text-sm rounded-md text-right bg-gray-100 text-custom-color "
-            placeholder="0"
-          />
-        </div>
-      </div>
-
-      <div>
-        <label htmlFor="selectTip" className="block text-sm font-medium text-gray-700 mb-2">Select Tip</label>
-        <div className="flex mb-4">
-          <button className="mr-4 px-4 py-2 bg-blue-500 text-white rounded-md">5%</button>
-          <button className="mr-4 px-4 py-2 bg-blue-500 text-white rounded-md">10%</button>
-          <button className="px-4 py-2 bg-blue-500 text-white rounded-md">15%</button>
-        </div>
-        <div className="flex mb-4">
-          <button className="mr-4 px-4 py-2 bg-blue-500 text-white rounded-md">25%</button>
-          <button className="mr-4 px-4 py-2 bg-blue-500 text-white rounded-md">50%</button>
-          <input type="text" className="border border-gray-300 rounded-md px-3 py-2 w-32" placeholder="Custom Tip" />
-        </div>
-      </div>
+    <div className="whitespace-no-wrap text-black mt-[-20rem] text-3xl uppercase">
+      <span className="tracking-wider">S</span>
+      <span className="tracking-wider">P</span>
+      <span className="tracking-wider">L</span>
+      <span className="tracking-wider">I</span>
+      <br />
+      <span className="tracking-wider">T</span>
+      <span className="tracking-wider">T</span>
+      <span className="tracking-wider">E</span>
+      <span className="tracking-wider">R</span>
     </div>
+
   );
 }
